dc9a23258cb8190d4de4e3c20d2237dacbde8ab4
Sandeep Bhatti <bhatti4u02in@gmail.com>
2015-01-10 15:31:12 +0530

    Added CKEditor

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.enabled = true
+    config.assets.precompile += Ckeditor.assets
   end
 end
