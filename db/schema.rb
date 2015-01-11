852f2f8ba87a40d5a5bd14b4d72155fdfaf1ec21
Sandeep Bhatti <bhatti4u02in@gmail.com>
2015-01-11 23:23:27 +0530

    Added username to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111174341) do
+ActiveRecord::Schema.define(version: 20150111175009) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150111174341) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id",                limit: 4,   default: 5
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
